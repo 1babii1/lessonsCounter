c7ff603da9e1262871af60f8fecdbe07ca39163a
babii <sabangulov.ilnaz@mail.ru>
2026-03-10 18:29:37 +0500

    fix

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // In production, this would be an environment variable
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "/api";
 
 const api = axios.create({
-    baseURL: API_URL,
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use(async (config) => {
-    const token = await AsyncStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = await AsyncStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
